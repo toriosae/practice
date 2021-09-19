d1667176b9036c545ca7cb439e96f1cc4b0f9113
Evan <75950224+toriosae@users.noreply.github.com>
2021-09-19 18:06:20 -0400

    Add files via upload

---
+++ assignment3web.js
@@ -0,0 +1,13 @@
+﻿
+
+
+function zipField() {
+
+var zipField = document.getElementById("zip").value;
+
+document.write(zipField);
+
+
+}
+
+document.getElementById("submit").addEventListener("click", zipField, false);
